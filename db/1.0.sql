8596014c6338539e2f5cffb73847c6ae78f36196
huadengwu <141378474+huadengwu@users.noreply.github.com>
2023-08-09 16:48:56 +0800

    数据库脚本更新

--- db/1.0.sql
+++ db/1.0.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `jjjshop_order_refund_image`
     MODIFY COLUMN `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' AFTER `app_id`;
+
+ALTER TABLE jjjshop_store_order MODIFY COLUMN `create_time`  timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
