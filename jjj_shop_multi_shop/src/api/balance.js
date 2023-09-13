ba7c9fc3bfceeeee9350ed4876c130a2a5368cf2
wxw850227 <279517889@qq.com>
2023-09-13 19:05:28 +0800

    升级v3
    升级v3

--- jjj_shop_multi_shop/src/api/balance.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-let BalanceApi = {
-  /*获取积分列表*/
-  getBalanceLog (data, errorback) {
-    return request._get('/shop/user.balance/log', data, errorback);
-  },
-}
-export default BalanceApi;
