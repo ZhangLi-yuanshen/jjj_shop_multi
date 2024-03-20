07cd5e6fdaa8e1b647db7a022ec4fa0e35190e5c
cookiemy <1946749843@qq.com>
2024-03-20 15:41:26 +0800

    端口

--- jjj_shop_multi_shop/vite.config.js
+++ jjj_shop_multi_shop/vite.config.js
@@ -14,7 +14,10 @@ export default defineConfig(({ mode }) => {
   return {
 	base: process.env.NODE_ENV === 'production' ? './' : '/',
     server: {
-      proxy: {
+		host: '0.0.0.0', //ip地址
+		port: 8080, //端口号
+		open: true, //启动后是否自动打开浏览器
+      	proxy: {
         '/api': {
           target: config.VITE_BASIC_URL,
           changeOrigin: true,
