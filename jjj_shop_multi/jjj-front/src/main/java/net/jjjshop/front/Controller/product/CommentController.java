eda76c3dbe25dcd8cf907a197cc87d1b8bd54d08
Z1737 <3608171498@qq.com>
2025-06-16 17:31:08 +0800

    店铺商品页

---
+++ jjj_shop_multi/jjj-front/src/main/java/net/jjjshop/front/Controller/product/CommentController.java
@@ -0,0 +1,35 @@
+package net.jjjshop.front.Controller.product;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import net.jjjshop.framework.common.api.ApiResult;
+import net.jjjshop.framework.log.annotation.OperationLog;
+import net.jjjshop.front.Controller.index.BaseController;
+import net.jjjshop.front.param.product.CommentPageParam;
+import net.jjjshop.front.service.product.ProductCommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@Api(value = "productComment", tags = {"商品评价"})
+@RestController
+@RequestMapping("/front/product/comment")
+public class CommentController extends BaseController {
+
+    @Autowired
+    private ProductCommentService productCommentService;
+
+    @RequestMapping(value = "/lists", method = RequestMethod.POST)
+    @OperationLog(name = "lists")
+    @ApiOperation(value = "lists", response = String.class)
+    public ApiResult<Map<String, Object>> lists(@Validated @RequestBody CommentPageParam commentPageParam) {
+        return ApiResult.ok(productCommentService.getList(commentPageParam));
+    }
+}
