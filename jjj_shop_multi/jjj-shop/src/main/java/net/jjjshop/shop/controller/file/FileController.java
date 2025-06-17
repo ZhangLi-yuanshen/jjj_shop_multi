2cef5f7eaf134cedc557cba5686d284e94e8b809
ZhaangLi <18072416561@163.com>
2025-06-17 13:13:48 +0800

    商城管理文件上传修正

--- jjj_shop_multi/jjj-shop/src/main/java/net/jjjshop/shop/controller/file/FileController.java
+++ jjj_shop_multi/jjj-shop/src/main/java/net/jjjshop/shop/controller/file/FileController.java
@@ -1,17 +1,18 @@
 package net.jjjshop.shop.controller.file;
 
-import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import net.jjjshop.common.entity.file.UploadGroup;
-import net.jjjshop.common.service.file.UploadFileService;
-
-import net.jjjshop.common.service.file.UploadGroupService;
 import net.jjjshop.framework.common.api.ApiResult;
+import net.jjjshop.framework.core.pagination.Paging;
+import net.jjjshop.framework.log.annotation.OperationLog;
+import net.jjjshop.shop.param.file.UploadFilePageParam;
+import net.jjjshop.shop.service.file.UploadFileService;
+import net.jjjshop.shop.service.file.UploadGroupService;
+import net.jjjshop.shop.vo.file.UploadFileVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,20 +43,47 @@ public class FileController {
     @RequestMapping(value = "/deleteGroup", method = RequestMethod.POST)
 
     public ApiResult<String> deleteGroup(@RequestParam Integer groupId){
-        if(uploadGroupService.deleteById(groupId)){
+        if(uploadGroupService.deleteGroup(groupId)){
             return ApiResult.ok(null,"删除成功");
         }else {
             return ApiResult.fail("删除失败");
         }
     }
     @RequestMapping(value = "/editGroup", method = RequestMethod.POST)
-    public ApiResult<String> editGroup(@RequestParam String groupId,@RequestParam String groupName){
+    public ApiResult<String> editGroup(@RequestParam Integer groupId,@RequestParam String groupName){
         if(uploadGroupService.editGroup(groupId,groupName)){
             return ApiResult.ok(null,"修改成功");
         }else {
             return ApiResult.fail("修改失败");
         }
     }
+    @RequestMapping(value = "/deleteFiles", method = RequestMethod.POST)
+    @OperationLog(name = "deleteFiles")
+    @ApiOperation(value = "deleteFiles", response = String.class)
+    public ApiResult<String> deleteFiles(String fileIds) throws Exception{
+        if(uploadGroupService.deleteFiles(fileIds)) {
+            return ApiResult.ok(null, "删除成功");
+        }else{
+            return ApiResult.fail("删除失败");
+        }
+    }
 
+    @RequestMapping(value = "/lists", method = RequestMethod.POST)
+    @OperationLog(name = "lists")
+    @ApiOperation(value = "lists", response = String.class)
+    public ApiResult<Paging<UploadFileVo>> lists(@Validated @RequestBody UploadFilePageParam uploadFilePageParam) throws Exception{
+        return ApiResult.ok(uploadFileService.getList(uploadFilePageParam));
+    }
+
+    @RequestMapping(value = "/moveFiles", method = RequestMethod.POST)
+    @OperationLog(name = "moveFiles")
+    @ApiOperation(value = "moveFiles", response = String.class)
+    public ApiResult<String> moveFiles(Integer groupId, String fileIds) throws Exception{
+        if(uploadGroupService.moveFiles(groupId, fileIds)) {
+            return ApiResult.ok(null, "移动成功");
+        }else{
+            return ApiResult.fail("移动失败");
+        }
+    }
 
 }
